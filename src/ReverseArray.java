8030d557ca443dc464e10a17256161d581d7d75c
Nhuz11 <nhuzbajra@gmail.com>
2024-06-03 20:58:48 +0545

    june 2

---
+++ src/ReverseArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr= {1,2,3,4,5,};
+        int l =arr.length;
+        int n =Math.floorDiv(l,2);
+        for(int i=0;i<n;i++){
+            int temp=arr[i];
+            arr[i]=arr[l-1-i];
+            arr[l-1-i]=temp;
+
+        }
+        for(int element:arr){
+            System.out.println(element);
+        }
+
+    }
+}
